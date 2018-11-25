4579fc16c89986b006dbbbeaffabd4f410c3d327
Олжас <@->
2018-11-25 16:15:46 +0600

    Добавьте файлы проекта.

---
+++ Проект4/Source.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+
+	/*int a, b, c, d, f, g;
+
+	a = 5;
+	b = 7;
+
+	c = a + b;
+	printf("%d\n", c);
+
+	d = a - b;
+	printf("%d\n", d);
+
+	f = a / b;
+	printf("%d\n", f);
+
+	g = a * b;
+	printf("%d\n", g);*/
+
+
+
+
+
+	/*int a, b, c,d,f,g;
+
+	a = 5;
+	b = 7;
+
+	c = pow(a, 2) + pow(b, 2);
+	printf("%d\n", c);
+
+	d = pow(a, 2) - pow(b, 2);
+	printf("%d\n", d);
+
+	f = pow(a, 2) / pow(b, 2);
+	printf("%d\n", f);
+
+	g = pow(a, 2) * pow(b, 2);
+	printf("%d\n", g);*/
+
+
+
+
+	/*float a, b,c;
+
+	a = 5.7;
+
+	b = 6.8;
+
+	c = a + b;
+	printf("%f\n", c);*/
+
+
+	/*int x, y, c;
+
+	x = 5;
+	y = 3;
+
+	c = (x - y) / 1 + (x*y);
+	printf("%d\n", c);*/
+
+	system("pause");
+
+}
